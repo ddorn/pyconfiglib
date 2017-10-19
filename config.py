7f6ebdb04b7c1aed05ad345a7474f8a2c9b156f5
ddorn <diego.dorn@free.fr>
2017-10-19 17:39:03 +1100

    better example

--- config.py
+++ config.py
@@ -8,15 +8,20 @@ class WallColors(configlib.SubConfig):
 
     east = (255, 0, 0)
     __east_type__ = conftypes.color
+    __east_hint__ = 'The color of the eastern wall, where the sun rises.'
 
     west = (0, 255, 0)
     __west_type__ = conftypes.color
+    __west_hint__ = 'The color of the western wall, where the cow bows.'
 
     nord = (0, 0, 255)
     __nord_type__ = conftypes.color
+    __nord_hint__ = 'The color of the northern wall, where the snow falls.'
+
 
     south = (0, 0, 0)
     __south_type__ = conftypes.color
+    __south_hint__ = 'The color of the southern wall, where the ice creams.'
 
 
 class Colors(configlib.SubConfig):
@@ -25,11 +30,15 @@ class Colors(configlib.SubConfig):
     text = (64, 128, 128)
     __text_type__ = conftypes.color
 
-    background = (255, 255, 255)
-    __background_type__ = conftypes.color
+    light = (255, 255, 255)
+    __light_type__ = conftypes.color
+    __light_hint__ = 'The color of your lights'
 
     walls = WallColors()
+    __walls_hint__ = 'The colors of the walls of your secret place'
 
+    castle = WallColors()
+    __castle_hint__ = 'The colors of the walls of your castle'
 
 class Config(configlib.Config):
     __config_path__ = 'config.json'
@@ -47,7 +56,7 @@ class Config(configlib.Config):
     __bald_hint__ = "Are you bald ?"
 
     colors = Colors()
-    default = Colors()
+    __colors_hint__ = 'The colors around you.'
 
 if __name__ == '__main__':
     configlib.update_config(Config)
