5a7561eea2f4dec942bdac3346ff6223dd961926
ddorn <diego.dorn@free.fr>
2017-10-20 10:40:41 +1100

    deploy --test

--- deploy.py
+++ deploy.py
@@ -7,19 +7,21 @@ import subprocess
 TYPES = ['major', 'minor', 'patch']
 
 
-def run(cmd):
+def run(cmd, test=False):
     click.secho(cmd, fg='yellow')
-    result = subprocess.Popen(cmd)
-    text = result.communicate()[0]
-    return result.returncode
-
+    if not test:
+        result = subprocess.Popen(cmd)
+        text = result.communicate()[0]
+        return result.returncode
+    return 0
 
 
 @click.command()
 @click.argument('type', type=click.Choice(TYPES))
 @click.argument('message', nargs=-1)
+@click.option('--test', '--preview', is_flag=True)
 @click.version_option(get_version())
-def main(type, message):
+def main(type, message, test):
     """Deploy a project easily and change the version number at the same time."""
 
     # read and parsing the version
@@ -63,16 +65,21 @@ def main(type, message):
 
     # we need to commit and push the change of the version number before everything
     # if we don't, travis will not have the right version and will fail to deploy
-    run('git commit -a -m "changing version number"'.format(message=message))
-    run('git push origin')
+
+    run('git commit -a -m "changing version number"'.format(message=message), test)
+    run('git push origin', test)
+
     if click.confirm('Are you sure you want to create a new release ?'):
         # creating a realase with the new version
         run('git tag v{version} -a -m "{message}"'.format(version=version,
-                                                          message=message))
-        run('git push origin --tags')
+                                                          message=message), test)
+        run('git push origin --tags', test)
 
         click.secho('Version changed to ' + version, fg='green')
     else:
+        revert_version()
+
+    if test:
         revert_version()
 
 
