74d05d65d738e53c27c2fc3121304822e9ac3228
ddorn <diego.dorn@free.fr>
2017-10-20 01:57:10 +1100

    minimal test: import

--- test/test_are_missing.py
+++ test/test_are_missing.py
@@ -1,2 +1,7 @@
 def test_needs_to_be_done():
-    assert True == 1
+    from configlib import config
+    conf = config.Config()
+    conf.__save__()
+    conf.__show__()
+    conf.__print_list__()
+
