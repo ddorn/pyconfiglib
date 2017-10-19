52d8c9b69f13d10673ed76ea8659827131d65fd6
ddorn <diego.dorn@free.fr>
2017-10-19 21:27:13 +1100

    corrected url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='configlib',
     version='1.0.0',
     packages=['configlib'],
-    url='www.github.com/ddorn/configlib',
+    url='https://github.com/ddorn/configlib',
     license='MIT',
     author='Diego Dorn',
     author_email='diego.dorn@free.fr',
