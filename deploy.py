b2c6d737dd933d4001d383a11b9ef429a1d4a6e0
ddorn <diego.dorn@free.fr>
2017-10-19 23:48:30 +1100

    comment deploy

--- deploy.py
+++ deploy.py
@@ -22,8 +22,10 @@ def run(cmd):
 def main(type, message):
     """Deploy a project easily and change the version number at the same time."""
 
+    # read and parsing the version
     version = get_version()
     version = list(map(int, version.split('.')))
+
     importance = TYPES.index(type)
     # we increase major/minor/path as chosen
     version[importance] += 1
@@ -31,13 +33,18 @@ def main(type, message):
     for i in range(importance + 1, 3):
         version[i] = 0
 
+    # save the version
     save_version(*version)
     version = get_version()
 
-    message = message and ' '.join(message) or 'Release of version %s' % version
+    # default message if nothing was provided
+    message = ' '.join(message) if message else 'Release of version %s' % version
 
+    # we need to commit and push the change of the version number before everything
+    # if we don't, travis will not have the right version and will fail to deploy
     run('git commit -a -m "changing version number"'.format(message=message))
     run('git push origin')
+    # creating a realase with the new version
     run('git tag v{version} -a -m "{message}"'.format(version=version,
                                                       message=message))
     run('git push origin --tags')
