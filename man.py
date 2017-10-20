a6d4976257910a85627c61e944a0455e4bfe8803
ddorn <diego.dorn@free.fr>
2017-10-21 07:53:20 +1100

    changed deploy to man and rethink architecture

--- man.py
+++ man.py
@@ -1,13 +1,16 @@
 import os
-
-import click
-from setup import get_version, save_version
 import subprocess
 
+import click
+
+from setup import get_version, save_version
+
 TYPES = ['major', 'minor', 'patch']
+TEST = False
 
 
 def run(cmd, test=False):
+    click.secho('$ ', fg='green', nl=0)
     click.secho(cmd, fg='yellow')
     if not test:
         result = subprocess.Popen(cmd)
@@ -16,24 +19,34 @@ def run(cmd, test=False):
     return 0
 
 
-@click.command()
-@click.argument('type', type=click.Choice(TYPES))
-@click.argument('message', nargs=-1)
-@click.option('--test', '--preview', is_flag=True)
+@click.group()
+@click.option('--test', is_flag=True)
 @click.version_option(get_version())
-def main(type, message, test):
-    """Deploy a project easily and change the version number at the same time."""
+def man(test):
+    global TEST
+    TEST = test
+
+@man.command()
+@click.argument('importance', type=click.Choice(TYPES))
+@click.argument('message', nargs=-1)
+@click.option('--test', is_flag=True)
+def release(importance, message, test):
+    """Deploy a project: update version, add tag, and push."""
+
+    global TEST
+    TEST = TEST or test
 
     # read and parsing the version
     version = get_version()
     click.secho('Current version: %s' % version, fg='green')
     version = list(map(int, version.split('.')))
     last_version = version[:]
+
     def revert_version():
         save_version(*last_version)
         click.secho('Version reverted to %s' % get_version(), fg='yellow')
 
-    importance = TYPES.index(type)
+    importance = TYPES.index(importance)
     # we increase major/minor/path as chosen
     version[importance] += 1
     # en reset the ones after
@@ -48,11 +61,11 @@ def main(type, message, test):
 
     # make sure it passes the tests
     if run('pytest') != 0:
-        click.secho("The test doesn't pass.", fg='red')
+        click.secho("The tests doesn't pass.", fg='red')
         revert_version()
         return
 
-    # make sur I can install it
+    # make sure I can install it
     if run('py setup.py install --user clean --all') != 0:
         click.secho('Failed to install the updated library.', fg='red')
         revert_version()
@@ -78,10 +91,30 @@ def main(type, message, test):
         click.secho('Version changed to ' + version, fg='green')
     else:
         revert_version()
+        return
 
     if test:
+        # We do not want to increase the version number at each test
         revert_version()
 
+@man.group()
+def add():
+    pass
+
+@add.command()
+@click.argument('lib')
+def dep(lib):
+    import importlib
+    modul = importlib.import_module(lib)
+    print(f'{lib}=={modul.__version__}')
+
+@add.command()
+@click.argument('filename')
+def file(filename):
+    dir = os.path.dirname(filename) or '.'
+    file = os.path.split(filename)[-1]
+    print(f'dir: {dir}')
+    print(f'file: {file}')
 
 if __name__ == '__main__':
-    main()
+    man()
