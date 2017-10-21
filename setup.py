6b52168e5e48ac099f7301e80bc91fc168f48af4
ddorn <diego.dorn@free.fr>
2017-10-21 18:49:40 +1100

    cleaned print

--- setup.py
+++ setup.py
@@ -35,8 +35,6 @@ if __name__ == '__main__':
     import manconfig
     config = manconfig.Config()
 
-    data_files = [(dir, list({file for patern in pats for file in glob.glob(patern)})) for (dir, pats) in config.data_files]
-    print(data_files)
     setup(
         name='pyconfiglib',
         version=get_version(),
@@ -49,5 +47,5 @@ if __name__ == '__main__':
         long_description=long_description,
         install_requires=config.dependancies,
         package_data=config.package_data,
-        data_files=data_files
+        data_files=[(dir, list({file for patern in pats for file in glob.glob(patern)})) for (dir, pats) in config.data_files]
     )
