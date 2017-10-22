fbcfa55906e8c6c1f0fe857fdb076bcf095b10c2
ddorn <diego.dorn@free.fr>
2017-10-22 11:31:08 +1100

    removed config from man.py

--- man.py
+++ man.py
@@ -8,31 +8,12 @@ import configlib
 import pypandoc
 
 from setup import get_version, save_version
+from manconfig import Config
 
 TYPES = ['major', 'minor', 'patch']
 TEST = False
 
 
-class Config(configlib.Config):
-    __config_path__ = os.path.join(os.path.dirname(__file__), 'manconfig.json')
-
-    libname = ''
-    github_username = ''
-    fullname = ''
-    email = ''
-    pypi_username = ''
-
-    package_data = dict()
-    __package_data_type__ = configlib.Python(dict)
-
-    data_files = []
-    __data_files_type__ = configlib.Python(list)
-
-    packages = []
-    __packages_type__ = configlib.Python(list)
-
-    dependancies = []
-    __dependancies_type__ = configlib.Python(list)
 
 
 CONFIG = Config()
