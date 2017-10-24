e7bdb5e881f9fef06a8d4db121d0dc6913319a6f
ddorn <diego.dorn@free.fr>
2017-10-24 16:50:52 +1100

    removed requirement 'man' that were creating cyclic imports at deployement time

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import glob
+import json
 
 from setuptools import setup
 
@@ -10,20 +11,20 @@ if __name__ == '__main__':
     except FileNotFoundError:
         long_description = 'Configuration for python made easy'
 
-    from man import ManConfig
-    config = ManConfig()
+    with open('.manconfig') as f:
+        config = json.load(f)
 
     setup(
         name='pyconfiglib',
-        version=str(config.version),
-        packages=config.packages,
+        version=config['version'],
+        packages=config['packages'],
         url='https://github.com/ddorn/configlib',
         license='MIT',
         author='Diego Dorn',
         author_email='diego.dorn@free.fr',
         description='Configuration for python made easy',
         long_description=long_description,
-        install_requires=config.dependancies,
-        package_data=config.package_data,
-        data_files=[(dir, list({file for patern in pats for file in glob.glob(patern)})) for (dir, pats) in config.data_files]
+        install_requires=config['dependancies'],
+        package_data=config['package_data'],
+        data_files=[(dir, list({file for patern in pats for file in glob.glob(patern)})) for (dir, pats) in config['data_files']]
     )
