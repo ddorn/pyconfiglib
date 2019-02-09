3ad01d0bb9344e18719d82a5928b4d8e5fe726ac
ddorn <diego.dorn@free.fr>
2019-02-09 15:52:08 +0100

    v1.3.3: not logging as root logger

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except FileNotFoundError:
 
 setup(
     name='pyconfiglib',
-    version='1.3.2',
+    version='1.3.3',
     packages=['configlib'],
     url='https://github.com/ddorn/pyconfiglib',
     license='MIT',
